49327abd92eeb543db0ac17cccb5844df85c6ac6
srimukha-sarma06 <srimukharajasarma@gmail.com>
2026-05-12 14:48:34 +0530

    Update sensordata.py

--- sensordata.py
+++ sensordata.py
@@ -1,7 +1,7 @@
 import mujoco
 import numpy as np
 
-model = mujoco.MjModel.from_xml_path('/media/srimukha-sarma/Windows-SSD/xtr_lair-main/src/robots/m2_metal_description/mujoco/flat_scene.xml')
+model = mujoco.MjModel.from_xml_path('flat_scene.xml')
 data = mujoco.MjData(model)
 
 joint_vel_start = model.sensor("FL_hip_velocity_sensor").id
