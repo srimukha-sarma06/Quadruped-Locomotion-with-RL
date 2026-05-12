07421cde0a7e122de71575e1dc38f600017b4032
srimukha-sarma06 <srimukharajasarma@gmail.com>
2026-05-12 14:45:49 +0530

    Delete env_checks.py

--- env_checks.py
+++
@@ -1,8 +0,0 @@
-import mujoco
-
-xml_path = '/media/srimukha-sarma/Windows-SSD/xtr_lair-main/src/robots/m2_metal_description/mujoco/flat_scene.xml'
-
-model = mujoco.MjModel.from_xml_path(xml_path)
-data = mujoco.MjData(model)
-
-print(data.contact.friction[:, :])
